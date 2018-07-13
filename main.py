99bd43c60b7e476e71d3a71f4a0695e67fe2ef1f
Zach Baylin <zbaylin@gmail.com>
2018-07-13 11:26:20 -0400

    refactored Company model

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 import pprint
 
-import requests
-from lxml import etree
 from util import db
-from models.company import Company
-
-
-def searchCompanies(searchTerm):
-  payload = {
-    "company": searchTerm,
-    "output": "atom"
-  }
-  r = requests.get("https://www.sec.gov/cgi-bin/browse-edgar", params=payload)
-  tree = etree.fromstring(r.content)
-
-  companyList = []
-  for company in tree.findall("entry/content/company-info", tree.nsmap):
-    companyItem = Company(
-      company.find("cik", company.nsmap).text,
-      company.find("name", company.nsmap).text
-    )
-    companyList.append(companyItem)
-
-  return companyList
+from models import company
 
 
 if __name__ == "__main__":
   pp = pprint.PrettyPrinter(indent=2)
   db.initDB()
 
-  companyList = searchCompanies(input("Please enter a company name: "))
+  companyList = company.searchCompanies(input("Please enter a company name: "))
 
   print("\n\nSelect a company from the list")
-  for i, company in enumerate(companyList):
-    print(i, company.name)
-  company = companyList[int(input("Company number: "))]
-  company.getFilings()
-
-  company.filings[0].getData()
-  for datum in company.filings[0].data:
-    pp.pprint(vars(datum))
+  for i, companyItem in enumerate(companyList):
+    print(i, companyItem.name)
+  companyItem = companyList[int(input("Company number: "))]
+  companyItem.getFilings()
--- models/company.py
+++ models/company.py
@@ -3,6 +3,25 @@ import lxml.etree as etree
 from models import filings
 
 
+def searchCompanies(searchTerm):
+  payload = {
+    "company": searchTerm,
+    "output": "atom"
+  }
+  r = requests.get("https://www.sec.gov/cgi-bin/browse-edgar", params=payload)
+  tree = etree.fromstring(r.content)
+
+  companyList = []
+  for company in tree.findall("entry/content/company-info", tree.nsmap):
+    companyItem = Company(
+      company.find("cik", company.nsmap).text,
+      company.find("name", company.nsmap).text
+    )
+    companyList.append(companyItem)
+
+  return companyList
+
+
 class Company:
   def __init__(self, cik, name, filings=[]):
     self.cik = cik
